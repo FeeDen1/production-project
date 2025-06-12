8619aaf416921688959556dedae9dc6fbe2abc4b
Denis <denisfedin1308@mail.ru>
2025-06-12 03:47:37 +0300

    gitignore fix

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,38 @@
+import webpack from "webpack";
+import {type} from "node:os";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+
+export function buildLoaders ({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const scssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]'
+                            : '[hash:base64:8]'
+                    }
+                }
+            },
+            'sass-loader'
+        ],
+    }
+
+    return [
+        typescriptLoader,
+        scssLoaders,
+    ]
+}
