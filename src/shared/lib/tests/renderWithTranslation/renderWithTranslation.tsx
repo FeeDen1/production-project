87683af1065702d3c5a11537a81c7519178a1327
Denis <denisfedin1308@mail.ru>
2025-10-14 22:39:29 +0300

    make lang switch async

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,5 +1,5 @@
 import {ReactNode} from "react";
-import {I18nextProvider, withTranslation} from "react-i18next";
+import {I18nextProvider} from "react-i18next";
 import {render} from "@testing-library/react";
 import i18nForTests from "shared/config/i18n/i18nForTests";
 
